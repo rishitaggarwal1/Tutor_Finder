514ae6497bd41fb1bd258721f45dc5478dd83879
rishitaggarwal1 <rishitaggarwal1@gmail.com>
2020-06-05 23:40:44 +0530

    Request Page Completed

--- my_request.php
+++ my_request.php
@@ -148,13 +148,19 @@ if (isset($_SESSION['user_id'])) {
 				  			if($row['is_accepted']==0)
 				  			{
 				  			?>
-				  				<td scope="col" style="width:15vw;vertical-align: top;"><button class="btn btn-danger btn-sm">Not Accepted</button></td>
+				  				<td scope="col" style="width:15vw;vertical-align: top;"><button class="btn btn-warning btn-sm">Not Seen</button></td>
+				  			<?php
+				  			}
+				  			else if($row['is_accepted']==1)
+				  			{
+				  			?>
+				  				<td scope="col" style="width:15vw;vertical-align: top;"><button class="btn btn-success btn-sm">Accepted</button></td>
 				  			<?php
 				  			}
 				  			else
 				  			{
 				  			?>
-				  				<td scope="col" style="width:15vw;vertical-align: top;"><button class="btn btn-success btn-sm">Accepted</button></td>
+				  				<td scope="col" style="width:15vw;vertical-align: top;"><button class="btn btn-danger btn-sm">Declined</button></td>
 				  			<?php
 				  			}
 				  		?>
@@ -170,8 +176,28 @@ if (isset($_SESSION['user_id'])) {
         	else if($type=="Tutor")
         	{
         	?>
-        		
-        	<div class="row">
+        <div class="row">
+        	<div class="col-lg-1 col-md-1"></div>
+        	<div class="col-lg-10 col-md-10 col-sm-12 col-12">
+			  <div class="nav-wrapper">
+			    <ul class="nav nav-pills nav-fill flex-column flex-md-row" id="tabs-text" role="tablist">
+			      <li class="nav-item">
+			        <a class="nav-link mb-sm-3 mb-md-0 active" id="tabs-text-1-tab" data-toggle="tab" href="#tabs-text-1" role="tab" aria-controls="tabs-text-1" aria-selected="true">New Request</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link mb-sm-3 mb-md-0" id="tabs-text-2-tab" data-toggle="tab" href="#tabs-text-2" role="tab" aria-controls="tabs-text-2" aria-selected="false">Accepted Requested</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link mb-sm-3 mb-md-0" id="tabs-text-3-tab" data-toggle="tab" href="#tabs-text-3" role="tab" aria-controls="tabs-text-3" aria-selected="false">Declined Requested</a>
+			      </li>
+			    </ul>
+			  </div>
+			  <br>
+			<div class="tab-content" id="myTabContent1">
+
+				<!-- New Request Details Tab -->
+				  <div class="tab-pane fade show active" id="tabs-text-1" role="tabpanel" aria-labelledby="tabs-text-1-tab">
+				  	<div class="row">
             	<table class="table table-hover">
 				  <thead class="text-center">
 				    <tr>
@@ -208,7 +234,99 @@ if (isset($_SESSION['user_id'])) {
 				  	?>
 				  </tbody>
 				</table>
-            </div>
+            	</div>
+				</div>
+
+				<!-- Accepted Details Tab -->
+				  <div class="tab-pane fade show active" id="tabs-text-2" role="tabpane1" aria-labelledby="tabs-text-2-tab">
+				  	<div class="row">
+		            	<table class="table table-hover">
+						  <thead class="text-center">
+						    <tr>
+						      <th scope="col" style="width:20vw;vertical-align: top;">Student Name</th>
+						      <th scope="col" style="width:20vw;vertical-align: top;">Mobile Number</th>
+						      <th scope="col" style="width:30vw;vertical-align: top;">Email Id</th>
+						      <th scope="col" style="width:15vw;vertical-align: top;">Accept</th>
+						      <th scope="col" style="width:15vw;vertical-align: top;">Decline</th>
+						    </tr>
+						  </thead>
+						  <tbody>
+						  	<?php
+						  		$sql="SELECT * from tution_request where tutor_id='$id' AND is_accepted=1;";
+						  		$result=mysqli_query($con,$sql);
+						  		$i=0;
+						  		while($row=mysqli_fetch_array($result,MYSQLI_ASSOC))
+						  		{
+						  			$i++;
+						  			$student_id=$row['student_id'];
+						  			$s="SELECT * from users where user_id='$student_id';";
+						  			$res=mysqli_query($con,$s);
+						  			$row1=mysqli_fetch_assoc($res);
+						  		?>
+						  		<tr class="text-center font_color_change">
+						  			<input type="text" name="student_id" value="<?php echo $student_id; ?>" id="student_id<?php echo $i; ?>" hidden>
+						  			<td class="font_color_change" scope="col" style="width:20vw;vertical-align: top;"><?php echo $row1['name']; ?></td>
+						  			<td scope="col" style="width:20vw;vertical-align: top;"><?php echo $row1['mobile']; ?></td>
+						  			<td scope="col" style="width:30vw;vertical-align: top;"><?php echo $row1['email_id']; ?></td>
+						  			<td scope="col" style="width:15vw;vertical-align: top;"><button class="btn btn-success btn-sm accept_button" id="A_<?php echo $i; ?>">Accept</button></td>
+						  			<td scope="col" style="width:15vw;vertical-align: top;"><button id="D_<?php echo $i; ?>" class="btn btn-danger btn-sm decline_button">Decline</button></td>
+						  		</tr>
+						  		<?php
+						  		}
+						  	?>
+						  </tbody>
+						</table>
+	            	</div>
+					</div>
+
+					<div class="tab-pane fade show active" id="tabs-text-3" role="tabpanel" aria-labelledby="tabs-text-3-tab">
+						<div class="row">
+		            	<table class="table table-hover">
+						  <thead class="text-center">
+						    <tr>
+						      <th scope="col" style="width:20vw;vertical-align: top;">Student Name</th>
+						      <th scope="col" style="width:20vw;vertical-align: top;">Mobile Number</th>
+						      <th scope="col" style="width:30vw;vertical-align: top;">Email Id</th>
+						      <th scope="col" style="width:15vw;vertical-align: top;">Accept</th>
+						      <th scope="col" style="width:15vw;vertical-align: top;">Decline</th>
+						    </tr>
+						  </thead>
+						  <tbody>
+						  	<?php
+						  		$sql="SELECT * from tution_request where tutor_id='$id' AND is_accepted=-1;";
+						  		$result=mysqli_query($con,$sql);
+						  		$i=0;
+						  		while($row=mysqli_fetch_array($result,MYSQLI_ASSOC))
+						  		{
+						  			$i++;
+						  			$student_id=$row['student_id'];
+						  			$s="SELECT * from users where user_id='$student_id';";
+						  			$res=mysqli_query($con,$s);
+						  			$row1=mysqli_fetch_assoc($res);
+						  		?>
+						  		<tr class="text-center font_color_change">
+						  			<input type="text" name="student_id" value="<?php echo $student_id; ?>" id="student_id<?php echo $i; ?>" hidden>
+						  			<td class="font_color_change" scope="col" style="width:20vw;vertical-align: top;"><?php echo $row1['name']; ?></td>
+						  			<td scope="col" style="width:20vw;vertical-align: top;"><?php echo $row1['mobile']; ?></td>
+						  			<td scope="col" style="width:30vw;vertical-align: top;"><?php echo $row1['email_id']; ?></td>
+						  			<td scope="col" style="width:15vw;vertical-align: top;"><button class="btn btn-success btn-sm accept_button" id="A_<?php echo $i; ?>">Accept</button></td>
+						  			<td scope="col" style="width:15vw;vertical-align: top;"><button id="D_<?php echo $i; ?>" class="btn btn-danger btn-sm decline_button">Decline</button></td>
+						  		</tr>
+						  		<?php
+						  		}
+						  	?>
+						  </tbody>
+						</table>
+	            	</div>
+					</div>
+
+			</div>
+
+
+			</div>
+
+		</div>
+
 
         	<?php
         	}
